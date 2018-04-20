1eb7fbc1de2df8f986261a583494e884a0bec2a4
Gabriel Krell <gabrielkrell@users.noreply.github.com>
2018-04-19 20:21:29 -0500

    Close DB conn after testing
    Oops

--- test.js
+++ test.js
@@ -17,3 +17,6 @@ con.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
 	  if (error) throw error;
 	  console.log('The solution is: ', results[0].solution);
 });
+con.end(function(err) {
+	console.log("Terminated db connection.")
+});
