a1086e374c222cf9483e863bc4d3cdaae4968817
Gabriel Krell <gabrielkrell@users.noreply.github.com>
2018-04-19 19:11:55 -0500

    Add test file
    This'll break Travis since it doesn't have a database.

---
+++ test.js
@@ -0,0 +1,19 @@
+'use strict';
+
+// test database connection
+
+var mysql = require('mysql');
+var con = mysql.createConnection({
+	  host: "localhost",
+	  user: "cs425",
+	  password: "cs425",
+	  database: "cs425"
+});
+con.connect(function(err) {
+	  if (err) throw err;
+	  console.log("Connected!");
+});
+con.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
+	  if (error) throw error;
+	  console.log('The solution is: ', results[0].solution);
+});
